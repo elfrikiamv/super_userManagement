e77b9ef8cdf43ab50359e5f21806ca4c002efaa2
elfrikiamv <elfrikiamv@gmail.com>
2024-09-11 13:36:57 -0600

    style: update view design > comments screen.

--- app/src/main/java/com/elfrikiamv/super_usermanagement/view/CommentsScreen.kt
+++ app/src/main/java/com/elfrikiamv/super_usermanagement/view/CommentsScreen.kt
@@ -3,16 +3,19 @@ package com.elfrikiamv.super_usermanagement.view
 // CommentsScreen.kt
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -21,6 +24,8 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -34,50 +39,87 @@ fun CommentsScreen(
     navController: NavController,
     viewModel: UserViewModel = viewModel()
 ) {
-    // Cambiar a observeAsState() para LiveData
+    // Obtenemos los comentarios desde el ViewModel utilizando LiveData y observeAsState
     val comments =
         viewModel.comments.observeAsState(emptyList()).value.filter { it.postId == postId }
 
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Commenstarios") },
+                title = { Text("Comentarios") },
                 navigationIcon = {
                     Icon(
                         imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = "Regresar",
-                        modifier = Modifier.clickable {
-                            navController.popBackStack() // Regresa a la pantalla anterior
-                        }
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .clickable { navController.popBackStack() }
                     )
                 }
             )
         },
         content = { paddingValues ->
-            LazyColumn(modifier = Modifier.padding(paddingValues)) {
-                items(comments) { comment ->
-                    CommentCard(comment)
-                }
-            }
+            CommentList(
+                comments = comments,
+                modifier = Modifier.padding(paddingValues)
+            )
         }
     )
 }
 
 @Composable
-fun CommentCard(comment: Comment) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 4.dp),
-        // Si deseas aplicar una elevación en Material 3, puedes usar este modificador
-        elevation = CardDefaults.elevatedCardElevation(4.dp), // Aplica la elevación de la sombra
-        // También puedes controlar el color de la sombra
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+fun CommentList(
+    comments: List<Comment>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(comments) { comment ->
+            CommentItem(comment)
+        }
+    }
+}
+
+@Composable
+fun CommentItem(comment: Comment) {
+    ElevatedCard(
+        modifier = Modifier.fillMaxWidth()
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = "Comentario: ${comment.name}")
-            Text(text = "Por: ${comment.email}")
-            Text(text = "Contenido: ${comment.body}")
+            Text(
+                text = comment.name,
+                style = MaterialTheme.typography.titleMedium,
+            )
+            HorizontalDivider(
+                modifier = Modifier.padding(vertical = 2.dp),
+                thickness = 1.dp
+            )
+            Text(
+                text = comment.body,
+                fontStyle = FontStyle.Italic,
+                modifier = Modifier.alpha(0.8f)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth() // Ocupa all el ancho disponible
+                    .padding(top = 8.dp),
+                horizontalArrangement = Arrangement.End // Alinea el contenido a la derecha
+            ) {
+                Text(
+                    text = "Por: ",
+                    fontStyle = FontStyle.Italic,
+                    modifier = Modifier
+                        .alignByBaseline()
+                        .alpha(0.8f)
+                )
+                Text(
+                    text = comment.email,
+                    modifier = Modifier.alignByBaseline()
+                )
+            }
         }
     }
 }
